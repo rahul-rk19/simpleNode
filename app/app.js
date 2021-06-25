d1b6df380167df4adfbf1677a94f7f53036bd765
Rahul Kumar <51615688+rahul-rk19@users.noreply.github.com>
2021-06-25 22:34:10 +0530

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+});
+
+app.get('/',(req,res,next )=>{
+  res.send("Hello from server");
+})
